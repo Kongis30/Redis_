7e3e3e920305c4d4a51a3db032acee0806cbaae3
Kongis30 <95322758+Kongis30@users.noreply.github.com>
2022-09-27 10:24:35 +0800

    Update RedisSpringbootDemoApplicationTests.java

--- src/test/java/com/kong/redis_springbootdemo/RedisSpringbootDemoApplicationTests.java
+++ src/test/java/com/kong/redis_springbootdemo/RedisSpringbootDemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RedisSpringbootDemoApplicationTests {
-    //test
+    //test1
     @Test
     void contextLoads() {
     }
