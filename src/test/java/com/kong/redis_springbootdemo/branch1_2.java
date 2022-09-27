fa33106b277df97db398858aa5fa4659d3807af7
kong <2020281063@email.szu.edu.cn>
2022-09-27 10:44:02 +0800

    update 1_2

---
+++ src/test/java/com/kong/redis_springbootdemo/branch1_2.java
@@ -0,0 +1,13 @@
+package com.kong.redis_springbootdemo;
+
+/**
+ * @Author: Kong
+ * @License: (C) Copyright 2005-2019, xxx Corporation Limited.
+ * @Contact: xxx@xxx.com
+ * @Date: 2022-09-27 10:43
+ * @Version: 1.0
+ * @Description:
+ */
+
+public class branch1_2 {
+}
